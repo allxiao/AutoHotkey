ad744f79563857bf1da2cd1fc74ecd6e8a119fe7
allxiao <arieshout@gmail.com>
2025-03-09 20:47:48 +0800

    Update script settings

--- AntimatterDimensions/AdHelper.js
+++ AntimatterDimensions/AdHelper.js
@@ -3,24 +3,15 @@ class AdHelper {
     this.intervalId = null;
   }
 
-  start() {
+  start(sacrificeThreshold = 3.0, maxGalaxies = 1, dimensionBoostLimit = 12) {
     this.stop();
     const _this = this;
     function group() {
-      _this._sacrifice(2.0);
-      _this._dimensionBoost();
-      _this._antimatterGalaxies(2);
-    }
-    this.intervalId = setInterval(group, 500);
-  }
-
-  // 挑战 8 特别配置，不需要 Animator Galaxies，等 3 倍以上再 Sacrifice
-  challenge8() {
-    const _this = this;
-    this.stop();
-    function group() {
-      _this._sacrifice(3.0);
-      _this._dimensionBoost();
+      _this._sacrifice(sacrificeThreshold);
+      // 挑战 8 的时候 Antimatter Galaxy 是禁用的，但是因为挑战结束自动退回到常规模式
+      // 所以这里所有情况都尝试 Galaxy Reset，以便无人监管的时候可以继续在常规模式下使用
+      _this._antimatterGalaxies(maxGalaxies);
+      _this._dimensionBoost(dimensionBoostLimit);
     }
     this.intervalId = setInterval(group, 500);
   }
@@ -29,9 +20,7 @@ class AdHelper {
     if (this.intervalId) {
       clearInterval(this.intervalId);
       this.intervalId = null;
-      console.log('停止检测');
-    } else {
-      console.log('没有检测在运行');
+      console.log('停止之前的检测');
     }
   }
 
@@ -59,7 +48,7 @@ class AdHelper {
   }
 
 
-  _dimensionBoost() {
+  _dimensionBoost(limit=10) {
     try {
       // 定位目标按钮元素
       const button = document.querySelector('div.dimboost button.o-primary-btn--dimension-reset');
@@ -67,11 +56,25 @@ class AdHelper {
       // 检查元素是否存在且未被禁用
       if (!button || button.classList.contains('o-primary-btn--disabled')) return;
 
-      // 检查按钮文本包含"unlock"关键词
-      if (button.textContent.includes('unlock')) {
-        button.click();
-        console.log('[成功点击 Dimension Boost] 当前按钮状态:', button.textContent);
+      const outerDiv = button.closest('div.dimboost');
+      if (!outerDiv) return;
+
+      const h4 = outerDiv.querySelector('h4');
+      if (!h4) return;
+
+      const text = h4.textContent;
+      const match = text.match(/(\d+)/);
+      if (!match) return;
+
+      // 转换为浮点数并判断条件
+      const value = parseInt(match[1]);
+      if (value >= limit) {
+        // 模拟按下点击这个按钮
+        return;
       }
+
+      button.click();
+      console.log('[成功点击 Dimension Boost] 当前按钮状态:', button.textContent);
     } catch (error) {
       console.error('[检测异常]', error);
     }
@@ -109,5 +112,5 @@ class AdHelper {
 }
 
 ad = new AdHelper();
-ad.challenge8();
-// ad.start();
+//ad.challenge8();
+ad.start();
